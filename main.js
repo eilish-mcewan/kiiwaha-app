6c70b6254aa9af0f1074015ed0a8ef58d5bb3420
Eilish McEwan <eilishmcewan@gmail.com>
2026-05-02 20:27:03 +1200

    added fetch() to get kīwaha from JSON file to display in console, edited main.js to show console in electron app

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ function createWindow() {
   });
 
   win.loadFile("index.html");
+  win.webContents.openDevTools({mode: "detach"});
 }
 
 app.whenReady().then(createWindow);
---
+++ script.js
@@ -0,0 +1,6 @@
+fetch('./models/kiiwaha.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => console.error('Error fetching data:', error));
