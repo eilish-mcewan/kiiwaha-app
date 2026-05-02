f378a57ea81005377f2e56d6af3c505d8bcb05b4
Eilish McEwan <eilishmcewan@gmail.com>
2026-05-02 20:50:26 +1200

    script.js fetches a random kīwaha each day and displays it + the definition

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 fetch('./models/kiiwaha.json')
   .then(response => response.json())
   .then(data => {
-    console.log(data)
+      const today = new Date()                                                                          
+      const day = today.getDate()        
+      const month = today.getMonth()                                  
+      const year = today.getFullYear()
+      const dateNumber = year * 10000 + month * 100 + day;
+      const index = dateNumber % data.length; 
+      const todaysKiiwaha = data[index];
+      const kiiwahaElement = document.getElementById('kiwaha');
+      const definitionElement = document.getElementById('definition');
+      kiiwahaElement.textContent = todaysKiiwaha.kīwaha;
+      definitionElement.textContent = todaysKiiwaha.definition;
+      console.log(todaysKiiwaha);
   })
   .catch(error => console.error('Error fetching data:', error));
